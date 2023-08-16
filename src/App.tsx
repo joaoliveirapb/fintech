46581b2b98624144f55b83fb3f863743d04f79f2
joaoliveirapb <joaoliveira.batista1@gmail.com>
2023-08-16 17:14:25 -0300

    feat: inserted the DataContext provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { Header } from './Components/Header'
 import { Sidenav } from './Components/Sidenav'
+import { DataContextProvider } from './Context/DataContext'
 import { Summary } from './Pages/Summary'
 import './Style.css'
 
 export default function App() {
   return (
-    <div>
-      <Sidenav />
-      <main>
-        <Header />
-        <Summary />
-      </main>
-    </div>
+    <DataContextProvider>
+      <div className="container">
+        <Sidenav />
+        <main>
+          <Header />
+          <Summary />
+        </main>
+      </div>
+    </DataContextProvider>
   )
 }
