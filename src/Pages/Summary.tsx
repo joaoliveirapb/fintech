a5b4fc40f9655ba5c7eeb492754293885abb9759
joaoliveirapb <joaoliveira.batista1@gmail.com>
2023-08-16 17:41:57 -0300

    feat: add to sales, received and processing boxes

--- src/Pages/Summary.tsx
+++ src/Pages/Summary.tsx
@@ -1,7 +1,41 @@
+import { useDataContext } from '../Context/DataContext'
+
 export function Summary() {
+  const { data } = useDataContext()
+
+  if (data === null) return null
   return (
-    <div>
-      <h1>Summary</h1>
-    </div>
+    <section>
+      <div className="summary flex mb">
+        <div className="box">
+          <h2>Vendas</h2>
+          <span>
+            {data
+              .filter((i) => i.status !== 'falha')
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
+          </span>
+        </div>
+        <div className="box">
+          <h2>Recebido</h2>
+          <span>
+            {data
+              .filter((i) => i.status === 'pago')
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
+          </span>
+        </div>
+        <div className="box">
+          <h2>Processando</h2>
+          <span>
+            {data
+              .filter((i) => i.status === 'processando')
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
+          </span>
+        </div>
+      </div>
+      <div className="box">Gráficos</div>
+    </section>
   )
 }
