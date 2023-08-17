ddcd6d7f62e2ffa91fec259eb83de3c6b28b367f
joaoliveirapb <joaoliveira.batista1@gmail.com>
2023-08-16 21:25:02 -0300

    feat: added routes with router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Header } from './Components/Header'
 import { Sidenav } from './Components/Sidenav'
 import { DataContextProvider } from './Context/DataContext'
+import { Sale } from './Pages/Sale'
+import { Sales } from './Pages/Sales'
 import { Summary } from './Pages/Summary'
 import './Style.css'
 
 export default function App() {
   return (
-    <DataContextProvider>
-      <div className="container">
-        <Sidenav />
-        <main>
-          <Header />
-          <Summary />
-        </main>
-      </div>
-    </DataContextProvider>
+    <BrowserRouter>
+      <DataContextProvider>
+        <div className="container">
+          <Sidenav />
+          <main>
+            <Header />
+            <Routes>
+              <Route path="/" element={<Summary />} />
+              <Route path="/vendas" element={<Sales />} />
+              <Route path="/vendas/:id" element={<Sale />} />
+            </Routes>
+          </main>
+        </div>
+      </DataContextProvider>
+    </BrowserRouter>
   )
 }
