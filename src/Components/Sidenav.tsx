3775dbfd099111cca694bc518664caae69ae5adb
joaoliveirapb <joaoliveira.batista1@gmail.com>
2023-08-16 21:30:32 -0300

    feat: added navigation links

--- src/Components/Sidenav.tsx
+++ src/Components/Sidenav.tsx
@@ -1,7 +1,56 @@
+import {
+  ArrowsDownUp,
+  EnvelopeSimple,
+  Gear,
+  House,
+  SignOut,
+  Storefront,
+} from '@phosphor-icons/react'
+import { NavLink } from 'react-router-dom'
+import { FintechLogo } from './FintechLogo'
+
 export function Sidenav() {
   return (
-    <div>
-      <h1>Sidenav</h1>
-    </div>
+    <nav className="sidenav box bg-3">
+      <FintechLogo title="Logo da Fintech" />
+      <ul>
+        <li>
+          <span>
+            <House size={24} />
+          </span>
+          <NavLink to="/">Resumo</NavLink>
+        </li>
+        <li>
+          <span>
+            <Storefront size={24} />
+          </span>
+          <NavLink to="/vendas">Vendas</NavLink>
+        </li>
+        <li>
+          <span>
+            <ArrowsDownUp size={24} />
+          </span>
+          <a>Webhooks</a>
+        </li>
+        <li>
+          <span>
+            <Gear size={24} />
+          </span>
+          <a>Configurações</a>
+        </li>
+        <li>
+          <span>
+            <EnvelopeSimple size={24} />
+          </span>
+          <a>Contato</a>
+        </li>
+        <li>
+          <span>
+            <SignOut size={24} />
+          </span>
+          <a>Sair</a>
+        </li>
+      </ul>
+    </nav>
   )
 }
