919047c27e58be462aa7d70ea45cfd1306668a8f
joaoliveirapb <joaoliveira.batista1@gmail.com>
2023-08-16 21:27:47 -0300

    feat: added title according to the page

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,16 +1,27 @@
-import { useDataContext } from '../Context/DataContext'
+import { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
 import { DateRange } from './DateRange'
 import { Months } from './Months'
 
 export function Header() {
-  const { data } = useDataContext()
+  const [title, setTitle] = useState('Resumo')
+  const location = useLocation()
 
-  console.log(data)
+  useEffect(() => {
+    if (location.pathname === '/') {
+      setTitle('Resumo')
+      document.title = 'Fintech | Resumo'
+    } else if (location.pathname === '/vendas') {
+      setTitle('Vendas')
+      document.title = 'Fintech | Vendas'
+    }
+  }, [location])
 
   return (
     <header className="mb">
-      <div className="mb">
+      <div className="daterange mb">
         <DateRange />
+        <h1 className="box bg-3">{title}</h1>
       </div>
       <Months />
     </header>
